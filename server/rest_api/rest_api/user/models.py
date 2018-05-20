5f1f18b062a21d681d4589fb10e99b1edfff584f
Brennan Gebotys <gebob19.git@gmail.com>
2018-05-20 19:02:17 -0400

    added user status

--- server/rest_api/rest_api/user/models.py
+++ server/rest_api/rest_api/user/models.py
@@ -4,8 +4,12 @@ from mongoengine import Document, fields
 class User(Document):
     firstName = fields.StringField(required=True)
     lastName = fields.StringField(required=True)
+
     email = fields.StringField(required=True, unique=True)
     password = fields.StringField(required=True)
+
+    # ENUM("AVAILABLE", "BUSY")
+    status = fields.StringField(required=True, default="BUSY") 
 
     # list of document IDs
     friends = fields.ListField(fields.StringField(), default=[])
