4203b9635bd1e328fd8baa5f1af4f65fb56aeddd
Brennan Gebotys <gebob19.git@gmail.com>
2018-05-20 11:43:41 -0400

    updated start.sh

--- server/start.sh
+++ server/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-node socket_server/index.js & \
+cd socket_server && \
+npm install && \
+cd .. && node socket_server/index.js & \
 source rest_api/venv/bin/activate && \
 pip install -r rest_api/requirements.txt > /dev/null && \
 python rest_api/manage.py runserver
