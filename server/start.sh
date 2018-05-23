18d95dd8aab22bab724b9c668de059ce61a05cea
Brennan Gebotys <gebob19.git@gmail.com>
2018-05-22 23:19:52 -0400

    update server automation

--- server/start.sh
+++ server/start.sh
@@ -3,6 +3,6 @@
 cd socket_server && \
 npm install && \
 cd .. && node socket_server/index.js & \
-source rest_api/venv/bin/activate && \
-pip install -r rest_api/requirements.txt > /dev/null && \
-python rest_api/manage.py runserver
+source rest_api/env/bin/activate && \
+pip3 install -r rest_api/requirements.txt > /dev/null && \
+python3 rest_api/manage.py runserver
